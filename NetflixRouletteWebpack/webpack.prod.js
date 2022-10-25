b7bcabe4a0b76889bd3e41d8b80b79b0e84943f3
mariia-soshnikova-EPAM <mariia_soshnikova@epam.com>
2022-10-26 00:26:41 +0400

    optimizing production build by adding minifiers

--- NetflixRouletteWebpack/webpack.prod.js
+++ NetflixRouletteWebpack/webpack.prod.js
@@ -2,6 +2,10 @@ const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -9,5 +13,34 @@ module.exports = merge(common, {
     filename: "main.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new CleanWebpackPlugin()],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          //Extracts CSS into files
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 });
