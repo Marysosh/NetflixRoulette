198dffd6c5ad06b9a5351fda305923c468840e21
mariia-soshnikova-EPAM <mariia_soshnikova@epam.com>
2022-10-25 23:47:11 +0400

    adding babel loader and clean-webpack-plugin

--- NetflixRouletteWebpack/webpack.common.js
+++ NetflixRouletteWebpack/webpack.common.js
@@ -17,6 +17,16 @@ module.exports = {
           "sass-loader",
         ],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
     ],
   },
 };
