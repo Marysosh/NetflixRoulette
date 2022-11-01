e938ba933d0b2a6870047b839f908a41dff1cc7b
mariia-soshnikova-EPAM <mariia_soshnikova@epam.com>
2022-11-01 22:13:58 +0400

    adding header

--- NetflixRouletteWebpack/src/components/search-panel/SearchPanel.js
+++ NetflixRouletteWebpack/src/components/search-panel/SearchPanel.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./SearchPanel.scss";
 
+import Header from "../header/Header";
 import SearchTitle from "../search-title/SearchTitle";
 import SearchForm from "../search-form/SearchForm";
 
 function SearchPanel() {
   return (
     <div className="search-panel">
+      <Header />
       <div className="search-panel__title">
         <SearchTitle />
       </div>
