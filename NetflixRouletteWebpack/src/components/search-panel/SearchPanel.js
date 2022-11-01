addcfd4f2d2ed49d6a243ef02c71cec73e2c116c
mariia-soshnikova-EPAM <mariia_soshnikova@epam.com>
2022-11-01 22:06:26 +0400

    adding search title

--- NetflixRouletteWebpack/src/components/search-panel/SearchPanel.js
+++ NetflixRouletteWebpack/src/components/search-panel/SearchPanel.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./SearchPanel.scss";
 
+import SearchTitle from "../search-title/SearchTitle";
 import SearchForm from "../search-form/SearchForm";
 
 function SearchPanel() {
   return (
     <div className="search-panel">
+      <div className="search-panel__title">
+        <SearchTitle />
+      </div>
       <div className="search-panel__search-form">
         <SearchForm />
       </div>
---
+++ NetflixRouletteWebpack/src/components/search-title/SearchTitle.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SearchTitle.scss";
+
+function SearchTitle() {
+  return <h2 className="search-title">Find your movie</h2>;
+}
+
+export default SearchTitle;
