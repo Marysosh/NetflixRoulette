353bd9e5b911386d08cf86096a463eea1b7029c7
mariia-soshnikova-EPAM <mariia_soshnikova@epam.com>
2022-12-13 01:25:55 +0400

    adding validation to Add movie modal

--- NetflixRouletteWebpack/src/components/modals/edit-movie-modal/EditMovieModal.js
+++ NetflixRouletteWebpack/src/components/modals/edit-movie-modal/EditMovieModal.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-useless-escape */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from "react";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 import PropTypes from "prop-types";
 
 import "./EditMovieModal.scss";
@@ -9,60 +11,14 @@ import crossIcon from "../close_button.png";
 import GenreDropdown from "../../genre-dropdown/GenreDropdown";
 
 const BASE_CLASS = "edit-movie-modal";
-
-// const placeHolders = {
-//   titlePlaceholder: "Moana",
-//   movieUrlPlaceholder:
-//     "https://image.tmdb.org/t/p/w500/3kcEGnYBHDeqmdYf8ZRbKdfmlUy.jpg",
-//   releaseDatePlaceholder: "Select Date",
-//   ratingPlaceholder: "7.8",
-//   runtimePlaceholder: "minutes",
-//   overviewPlaceholder: "Movie description",
-// };
 function EditMovieModal({
   handleEditModalOpen,
   handleMovieEdit,
   modalTitle,
   showCongratsModal,
-  // editingValues,
 }) {
   const [selectedGenres, setSelectedGenres] = useState("");
 
-  // const onFormSubmit = (e) => {
-  //   e.preventDefault();
-  //   const data = new FormData(e.target);
-
-  //   handleMovieEdit({
-  //     title: data.get("title"),
-  //     genre: selectedGenres,
-  //     releaseDate: data.get("release-date"),
-  //     rating: data.get("rating"),
-  //     runtime: data.get("runtime"),
-  //     image: data.get("movie-url"),
-  //     overview: data.get("overview"),
-  //   });
-
-  //   if (modalTitle === "Add movie") {
-  //     showCongratsModal(true);
-  //   }
-  // };
-
-  // const {
-  //   titlePlaceholder,
-  //   movieUrlPlaceholder,
-  //   releaseDatePlaceholder,
-  //   ratingPlaceholder,
-  //   runtimePlaceholder,
-  //   overviewPlaceholder,
-  // } = placeHolders;
-  // const {
-  //   titleValue,
-  //   movieUrlValue,
-  //   releaseDateValue,
-  //   ratingValue,
-  //   runtimeValue,
-  //   overviewValue,
-  // } = editingValues;
   const formik = useFormik({
     initialValues: {
       title: "My custom title",
@@ -73,6 +29,25 @@ function EditMovieModal({
       runtime: "100",
       overview: "My overview",
     },
+    validationSchema: Yup.object({
+      title: Yup.string().required("Title is required"),
+      movieURL: Yup.string()
+        .matches(
+          /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/,
+          "Add valid URL"
+        )
+        .required("Movie URL is required")
+        .typeError("Movie URL must be valid"),
+      releaseDate: Yup.string()
+        .matches(
+          /^\d{4}-([0]\d|1[0-2])-([0-2]\d|3[01])$/,
+          "Use this format: YYYY-MM-DD"
+        )
+        .required("Release date is required"),
+      rating: Yup.number().typeError("Rating must be a number"),
+      runtime: Yup.number().typeError("Runtime must be a number"),
+      overview: Yup.string().required("Overview is required"),
+    }),
     onSubmit: (values) => {
       if (modalTitle === "Add movie") {
         handleMovieEdit({ ...values, genres: selectedGenres });
@@ -80,6 +55,9 @@ function EditMovieModal({
       } else {
         alert(JSON.stringify(values, null, 2));
       }
+    },
+    onReset: () => {
+      setSelectedGenres([]);
     },
   });
 
@@ -97,6 +75,7 @@ function EditMovieModal({
         <form
           className={`${BASE_CLASS}-inputs-container`}
           onSubmit={formik.handleSubmit}
+          onReset={formik.handleReset}
         >
           <div className="columns">
             <div className="left-column">
@@ -112,6 +91,10 @@ function EditMovieModal({
                 value={formik.values.title}
                 autoComplete="off"
               />
+              <div className="validation-error">
+                {formik.errors.title ? formik.errors.title : null}
+              </div>
+
               <label className="label" htmlFor="movie-url">
                 Movie URL
               </label>
@@ -124,8 +107,15 @@ function EditMovieModal({
                 value={formik.values.movieURL}
                 autoComplete="off"
               />
+              <div className="validation-error">
+                {formik.errors.movieURL ? formik.errors.movieURL : null}
+              </div>
+
               <label className="label">Genre</label>
               <GenreDropdown setSelectedGenres={setSelectedGenres} />
+              <div className="validation-error">
+                {selectedGenres.length ? null : "Choose at least 1 genre"}
+              </div>
             </div>
             <div className="right-column">
               <label className="label" htmlFor="release-date">
@@ -140,6 +130,9 @@ function EditMovieModal({
                 value={formik.values.releaseDate}
                 autoComplete="off"
               />
+              <div className="validation-error">
+                {formik.errors.releaseDate ? formik.errors.releaseDate : null}
+              </div>
 
               <label className="label" htmlFor="rating">
                 Rating
@@ -153,6 +146,9 @@ function EditMovieModal({
                 value={formik.values.rating}
                 autoComplete="off"
               />
+              <div className="validation-error">
+                {formik.errors.rating ? formik.errors.rating : null}
+              </div>
 
               <label className="label" htmlFor="runtime">
                 Runtime
@@ -166,6 +162,9 @@ function EditMovieModal({
                 value={formik.values.runtime}
                 autoComplete="off"
               />
+              <div className="validation-error">
+                {formik.errors.runtime ? formik.errors.runtime : null}
+              </div>
             </div>
           </div>
           <div className="overview">
@@ -181,6 +180,9 @@ function EditMovieModal({
               value={formik.values.overview}
               autoComplete="off"
             />
+            <div className="validation-error">
+              {formik.errors.overview ? formik.errors.overview : null}
+            </div>
           </div>
           <div className="form-buttons">
             <input className="reset-button" type="reset" value="Reset" />
